d0ea4e833f79fb8e0dae6984599488d25a145660
madhanrajoff <62301615+madhanrajoff@users.noreply.github.com>
2023-04-06 21:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class OutBound(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
 
-class HPPY:
+class Hoop:
     @classmethod
     def loop(cls, duration, path, l_path):
         cmd = ''
